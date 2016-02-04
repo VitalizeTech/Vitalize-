21b0340bcc27e886319bf3715163179bdc5f3473
Aaron Chau <aaronchauchau@gmail.com>
2016-02-04 14:32:10 -0800

    Added sliding menu library.

--- app/src/main/java/com/chris/scrim/MapsActivity.java
+++ app/src/main/java/com/chris/scrim/MapsActivity.java
@@ -14,6 +14,7 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
