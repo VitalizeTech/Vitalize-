4f1387f9fddf0eea0ae701483825ad2fcc27da8a
Phi Phi <phijanas@gmail.com>
2016-02-18 13:49:24 -0800

    Created register layout and also popup layout.

--- app/src/main/java/com/chris/scrim/RegisterActivity.java
+++ app/src/main/java/com/chris/scrim/RegisterActivity.java
@@ -2,12 +2,19 @@ package com.chris.scrim;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
-
+    private EditText myEmail, myPassword, myPassConfirmation;
+    private Button myRegButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        myEmail = (EditText) findViewById(R.id.regEmail);
+        myPassword = (EditText) findViewById(R.id.regPassword);
+        myPassConfirmation = (EditText) findViewById(R.id.regPassConfirm);
+        myRegButton = (Button) findViewById(R.id.regRegBtn);
     }
 }
